e4b7c4c3c610c4223bc33e24b6d991b1143ec66a
Sidoupiar <sidoupiar@qq.com>
2023-03-22 19:27:20 +0800

    建造限制功能

--- package/0013_BuildLimit/4_control.lua
+++ package/0013_BuildLimit/4_control.lua
@@ -9,10 +9,21 @@
 -- ----------------------------------------
 -- 初始化
 -- ----------------------------------------
+SINeed( "System/SIBuildLimit" )
+SIGlobal.CreateSettings( SIBuildLimit.Settings )
 
 -- ----------------------------------------
 -- 事件
 -- ----------------------------------------
+SIEventBus
+.Init( function( functionID )
+	-- 初始化数据
+	SIBuildLimit.InitData()
+end )
+.ConfigurationChange( function( functionID )
+	-- 初始化数据
+	SIBuildLimit.InitData()
+end )
 
 -- ------------------------------------------------------------------------------------------------
 -- ---------- 操作事件 ----------------------------------------------------------------------------
@@ -21,6 +32,66 @@
 -- ----------------------------------------
 -- 事件
 -- ----------------------------------------
+SIEventBus
+.Add( SIEvents.on_built_entity , function( event , functionID )
+	local entity = event.created_entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.BuildEntity( entity )
+end )
+.Add( SIEvents.on_robot_built_entity , function( event , functionID )
+	local entity = event.created_entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.BuildEntity( entity )
+end )
+.Add( SIEvents.on_trigger_created_entity , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.BuildEntity( entity )
+end )
+.Add( SIEvents.script_raised_built , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.BuildEntity( entity )
+end )
+.Add( SIEvents.on_player_mined_entity , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.DestroyEntity( entity )
+end )
+.Add( SIEvents.on_robot_mined_entity , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.DestroyEntity( entity )
+end )
+.Add( SIEvents.on_entity_died , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.DestroyEntity( entity )
+end )
+-- .Add( SIEvents.on_entity_destroyed , function( event , functionID )
+-- 	SIBuildLimit.DestroyEntity( entity )
+-- end )
+.Add( SIEvents.script_raised_destroy , function( event , functionID )
+	local entity = event.entity
+	if not entity or not entity.valid then
+		return
+	end
+	SIBuildLimit.DestroyEntity( entity )
+end )
 
 -- ------------------------------------------------------------------------------------------------
 -- ---------- 外部接口 ----------------------------------------------------------------------------
