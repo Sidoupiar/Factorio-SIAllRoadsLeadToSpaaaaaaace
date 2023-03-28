87a5de2577c2310ed60d5ee889a07f6fa38d757d
Sidoupiar <sidoupiar@qq.com>
2023-03-28 22:26:57 +0800

    新增矿物

--- package/0101_Resource/1_data_ore.lua
+++ package/0101_Resource/1_data_ore.lua
@@ -15,10 +15,10 @@ local oreList =
 	OreFour   = "四彩金" ,
 	OreUpper  = "上界碤"
 }
+local miningResultList = {}
 
 SIGen.SetGroup( SIConstants_Resource.raw.Groups.Resource.Ore )
 for oreID , oreAlias in pairs( oreList ) do
-	local projectileID = nil
 	SIGen
 	.New( SICommon.Types.Entities.Projectile , oreID , "扔出去的" .. oreAlias )
 	.MakeIcon( SICommon.Types.Items.Capsule , oreAlias )
@@ -84,9 +84,6 @@ for oreID , oreAlias in pairs( oreList ) do
 			}
 		}
 	}
-	.AddFunction( function( prototypeID , prototypeData )
-		projectileID = prototypeID
-	end )
 	.New( SICommon.Types.Items.Capsule , oreID , oreAlias ,
 	{
 		stack_size = 6000 ,
@@ -113,7 +110,7 @@ for oreID , oreAlias in pairs( oreList ) do
 							{
 								{
 									type = "projectile" ,
-									projectile = projectileID ,
+									projectile = SIConstants_Resource.raw.Entities[oreID] ,
 									starting_speed = 0.25 ,
 									target_effects =
 									{
@@ -131,4 +128,135 @@ for oreID , oreAlias in pairs( oreList ) do
 		}
 	} )
 	.AutoIcon()
+	table.insert( miningResultList ,
+	{
+		type = SICommon.Types.Items.Item ,
+		name = SIConstants_Resource.raw.Items[oreID] ,
+		probability = 0.001 ,
+		amount_min = 0 ,
+		amount_max = 200 ,
+		catalyst_amount = 200
+	} )
 end
+
+for oreID , oreAlias in pairs( oreList ) do
+end
+
+SIGen
+.New( SICommon.Types.Entities.Resource , "OreBase" , "矿山岩" )
+.AutoIcon()
+.SetSize( 1 , 1 )
+.Append
+{
+	flags = { SICommon.Flags.Entity.NotOnMap } ,
+	minable =
+	{
+		mining_time = 12.0 ,
+		mining_particle = "stone-particle" ,
+		results = miningResultList
+	} ,
+	map_color = { r = 1.00 , g = 1.00 , b = 1.00 } ,
+	friendly_map_color = { r = 1.00 , g = 1.00 , b = 1.00 } ,
+	enemy_map_color = { r = 1.00 , g = 1.00 , b = 1.00 } ,
+	mining_visualisation_tint = { r = 0.98 , g = 0.88 , b = 0.65 } ,
+	tree_removal_probability = 0.6 ,
+    tree_removal_max_distance = 1024 ,
+	autoplace =
+	{
+
+	} ,
+	stage_counts = { 100000 , 30000 , 10000 , 3000 , 1000 , 300 , 100 , 30 } ,
+	stages =
+	{
+		sheet =
+		{
+
+		}
+	} ,
+	mining_sound =
+	{
+		{
+			filename = "__core__/sound/axe-mining-ore-1.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-2.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-3.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-4.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-5.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-6.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-7.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-8.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-9.ogg" ,
+			volume = 0.8
+		} ,
+		{
+			filename = "__core__/sound/axe-mining-ore-10.ogg" ,
+			volume = 0.8
+		}
+	} ,
+	walking_sound =
+	{
+		{
+			filename = "__base__/sound/walking/resources/ore-01.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-02.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-03.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-04.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-05.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-06.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-07.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-08.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-09.ogg" ,
+			volume = 0.7
+		} ,
+		{
+			filename = "__base__/sound/walking/resources/ore-10.ogg" ,
+			volume = 0.7
+		}
+	}
+}
