26a1d4838ce1ea57d7fb2da2c63f7545da6a2ba5
Sidoupiar <sidoupiar@qq.com>
2023-04-04 12:21:01 +0800

    调试工具

--- package/0000_Core/4_control.lua
+++ package/0000_Core/4_control.lua
@@ -1315,9 +1315,11 @@ end )
 			local totalCount = #event.tiles
 			for index , tile in pairs( event.tiles ) do
 				if #surface.find_entities_filtered{ area = { { tile.position.x , tile.position.y } , { tile.position.x+1 , tile.position.y+1 } } } < 1 then
-					surface.set_tiles{ { name = "deepwater" , position = tile.position } }
-					if math.random() < 0.07 or index == totalCount then
-						table.insert( positionList , { tile.position.x + 0.5 , tile.position.y + 0.5 } )
+					if surface.get_tile( tile.position.x , tile.position.y ).name ~= "deepwater" then
+						surface.set_tiles{ { name = "deepwater" , position = tile.position } , false , false , true , false }
+						if math.random() < 0.07 or index == totalCount then
+							table.insert( positionList , { tile.position.x + 0.5 , tile.position.y + 0.5 } )
+						end
 					end
 				end
 			end
@@ -1361,7 +1363,7 @@ end )
 		if SIPermission.HasPermission( SIPermission.PermissionIDs.DeleteMap , playerIndex ) then
 			local surface = game.get_player( playerIndex ).surface
 			for index , tile in pairs( event.tiles ) do
-				surface.set_tiles{ { name = "refined-concrete" , position = tile.position } }
+				surface.set_tiles{ { name = "refined-concrete" , position = tile.position } , false , false , true , false }
 			end
 		else
 			SIPrint.Alert( playerIndex , SIPermission.GetPermissionMessage( SIPermission.PermissionIDs.DeleteMap ) )
