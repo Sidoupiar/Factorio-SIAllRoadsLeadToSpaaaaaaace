c9dee1078b5a721040fe9a48401bcb24030e80b2
Sidoupiar <sidoupiar@qq.com>
2024-11-05 15:12:48 +0800

    迁移

---
+++ package/0004_QuickStart/0_ConstantsData.lua
@@ -0,0 +1,13 @@
+local constantsData =
+{
+	ID = "QuickStart" ,
+	Code = "QuickStart" ,
+	Show = "快速开局" ,
+	FileList =
+	{
+		[SIInit.StateCodeDefine.DataFinalFixes] = { "3_data-final-fixes" } ,
+		[SIInit.StateCodeDefine.Control]        = { "4_control" }
+	}
+}
+
+return constantsData
